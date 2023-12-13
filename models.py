ee41967352e9cc2d395c3fce7d09a0138cccd7d0
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2023-12-13 17:48:13 +0200

    Update models.py

--- models.py
+++ models.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 from custom_layers import genSynthesis_block, Dis_block, style_map
 class Generator(nn.Module):
-  def __init__(self, MLP_num, in_style, channels, in_channels):
+    def __init__(self, MLP_num, in_style, channels, in_channels):
       super().__init__()
       self.in_style   = in_style
       self.style_map  = style_map(in_style, MLP_num)
       self.gen_blocks = nn.ModuleList([genSynthesis_block(in_style, channels[0], channels[1], first_block = True)])
       self.channels   = channels
       self.num_of_blocks = 1
-  def forward(self, z, alpha = 1, style_mixing = None, z2 = None):
+    def forward(self, z, alpha = 1, style_mixing = None, z2 = None):
       gen = None
       assert style_mixing == None or z2 != None,'assign values to z2'
       style_mixing = style_mixing if style_mixing else self.num_of_blocks
@@ -26,18 +26,18 @@ class Generator(nn.Module):
            gen = self.gen_blocks[-1].to_rgb(self.gen_blocks[-1](gen,w[-1]))
 
       return gen
-  def grow(self,):
+    def grow(self,):
       self.gen_blocks.append(genSynthesis_block(self.in_style, self.channels[self.num_of_blocks], self.channels[self.num_of_blocks + 1]))
       self.num_of_blocks += 1
 
     
  class Discriminator(nn.Module):
-  def __init__(self, channels, in_channels):
+    def __init__(self, channels, in_channels):
       super().__init__()
       self.channels   = channels
       self.dis_blocks = nn.ModuleList([Dis_block(channels[1], channels[0], last_block = True)])
       self.num_of_blocks = 1
-  def forward(self, x, alpha = 0.2):
+    def forward(self, x, alpha = 0.2):
       input = x
       x = self.dis_blocks[-1].from_rgb(x)
       x = self.dis_blocks[-1](x)
@@ -48,6 +48,6 @@ class Generator(nn.Module):
         for block_i in reversed(range(0, self.num_of_blocks - 1)):
             x = self.dis_blocks[block_i](x)
       return x
-  def grow(self,):
+    def grow(self,):
       self.dis_blocks.append(Dis_block(self.channels[self.num_of_blocks + 1], self.channels[self.num_of_blocks]))
       self.num_of_blocks += 1
