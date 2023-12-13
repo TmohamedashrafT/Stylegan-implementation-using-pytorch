e0398e5dae00bc387323f01afe5e2f2fa20ffbcf
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2023-12-13 18:57:48 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def visualize_output(fake_imgs):
   plt.figure(figsize=(10,10))
   plt.imshow(np.transpose(vutils.make_grid(fake_imgs.detach().cpu(), normalize=True),(1,2,0)))
-  plt.imshow()
+  plt.show()
 
 def get_style_mixing(batch_size, in_style, device, num_layers = 1, prop = 0.8):
   style_mixing, z = (None, None) if random.random() > prop else (num_layers, torch.randn(batch_size, in_style, device = device))
